0c2da3ceffefe08f9e5e38e4bc0b5a6ab608f5ad
anniekellner <annie.kellner@colostate.edu>
2021-08-30 10:47:55 -0600

    created tibble for akde data

---
+++ job_akde_estimates.R
@@ -0,0 +1,23 @@
+
+library(ctmm)
+library(dplyr)
+library(stringr)
+
+pb <- readRDS('./Data/bears_072321.Rds')
+
+pb <- pb %>%
+  select(id, datetime, gps_lon, gps_lat, X, Y)
+
+colnames(pb) <- c("id", "timestamp", "longitude", "latitude", "x", "y") # Movebank format
+
+pbt <- as.telemetry(pb)
+
+akde <- list()
+
+for(i in 1:length(pbt)){ # plots
+  pbx = pbt[[i]]
+  m.ouf = ctmm.guess(pbx,interactive=FALSE)
+  M.OUF = ctmm.fit(pbx, m.ouf) # model accouting for autocorrelation
+  UD2w = akde(pbx, M.OUF, weights=TRUE) # with optimal weighting of data
+  akde[[i]] <- summary(UD2w)
+}

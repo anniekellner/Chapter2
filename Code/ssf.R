9a7ee39cecdd3c4409229c246d7f736c6dcd5ae3
Annie <annie.kellner@colostate.edu>
2021-10-29 13:43:51 -0600

    save 1 hr fix rate points with true/false to folder for GEE

--- Code/ssf.R
+++ Code/ssf.R
@@ -16,24 +16,33 @@ rm(list = ls())
 
 steps <- readRDS('./Data/Derived-data/step_lengths.Rds') # step length data from Step_length.R
 
-# Covariates
+steps1 <- steps[[1]] # subset one hr fix rate to start (do other rates later)
+ssf1 <- steps1 %>% group_by(id) %>% random_steps(n_control = 15) # add random steps
+
+# Plot random v matched points
+ggplot(ssf1, aes(x2_, y2_, color=case_))+geom_point()+facet_wrap(~id, scales="free") # plot of random v. matched points
+
+# Convert back to sf object so can match up with spatial covariate data
+
+ssf1_sf <- st_as_sf(ssf1, coords = c('x2_', 'y2_'), crs = 3338)
+
+# ----------------- Bonepile ----------------------------------------------------------------- #
+
+bone <- st_read('./Data/Spatial/Bonepiles/bonepiles.shp') 
+bone <- st_transform(bone, 3338)
+
+ssf1_covs <- ssf1_sf %>%
+  group_by(id) %>%
+  mutate(dist2bonepile = st_distance(ssf1_sf, bone), by_element = TRUE)
 
 mean(steps1$sl_) # calculate mean step length for buffer. 500m should work 
 
 veg <- st_read('D:/Polar Bears/Data/Chapter2/veg/aga_arctic_ak_geobotanical_shp/aga_arctic_ak_geobotanical.shp')
 veg_aa <- st_transform(veg, 3338)
 
-steps1 <- steps[[1]]
 
-ssf1 <- steps1 %>% group_by(id) %>% random_steps(n_control = 15)
 
-# Plot random v matched points
 
-ggplot(ssf1, aes(x2_, y2_, color=case_))+geom_point()+facet_wrap(~id, scales="free") # plot of random v. matched points
-
-# Convert back to sf object so can match up with spatial covariate data
-
-ssf1_sf <- st_as_sf(ssf1, coords = c('x2_', 'y2_'), crs = 3338)
 
 test <- st_intersection(ssf1_sf, veg_aa)
 t

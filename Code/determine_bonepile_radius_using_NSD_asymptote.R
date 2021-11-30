88bb743f20adb961f468d3eba325bed0f855a953
anniekellner <annie.kellner@colostate.edu>
2021-11-30 09:52:52 -0700

    all bears dates added

--- Code/determine_bonepile_radius_using_NSD_asymptote.R
+++ Code/determine_bonepile_radius_using_NSD_asymptote.R
@@ -33,18 +33,6 @@ akde <- readRDS('./Data/akde_df.Rds')
 
 bp_only <- traj.df %>%
   filter(id == "pb_20525.2013" | id == "pb_20525.2014" | id == "pb_20586.2008" | id == "pb_32366.2014") %>%
-  inner_join(akde)
-
-bp1 <- bp_only %>%
-  filter(median_fix == 1)
-
-bp2 <- bp_only %>%
-  filter(median_fix == 2)
-
-mean(sqrt(bp1$R2n)) # 425 m
-sd(bp1$dist, na.rm = TRUE) * 2 # 476.7 m
-mean(sqrt(bp2$R2n)/2) # 727.0
-sd(bp2$dist/2, na.rm = TRUE) * 2 # 598 m
 
 # Bears that travel to bonepile
 
@@ -56,11 +44,41 @@ pb20966 <- traj.df %>%
   filter(id == "pb_20966.2008") %>%
   filter(date > "2008-08-26 16:00:00")
 
-trav <- traj.df %>%
+pb20492 <- traj.df %>%
+  filter(id == "pb_20492.2008") %>%
+  filter(date < "2008-10-16")
+
+pb20520 <- traj.df %>% 
+  filter(id == "pb_20520.2012") %>%
+  filter(date > "2012-08-27 20:00:00")
+
+pb20735 <- traj.df %>%
+  filter(id == "pb_20735.2009") %>%
+  filter(date > "2009-08-09 22:00:00" & date < "2009-08-29 21:00:00" | date > "2009-09-16 01:00:00")
+
+pb20845 <- traj.df %>%
+  filter(id == "pb_20845.2015") %>%
+  filter(date > "2015-09-23 20:01:29" & date < "2015-10-02 15:00:09")
+
+pb21015 <- traj.df %>%
+  filter(id == "pb_21015.2013") %>%
+  filter(date > "2013-08-20 08:00:31" & date < "2013-09-27 04:00")
+
+pb21368 <- traj.df %>%
   filter(id == "pb_21368.2014") %>%
-  filter(date > "2014-08-26 04:00:00") %>%
-  bind_rows(pb06810, pb20966) %>%
-  inner_join(akde)
+  filter(date > "2014-08-26 04:00:00") 
+
+pb32282 <- traj.df %>%
+  filter(id == "pb_32282.2008") %>%
+  filter(date > "2008-08-31 16:00:00")
+
+pb32366_2011 <- traj.df %>%
+  filter(id == "pb_32366.2011") %>%
+  filter(date > "2011-08-30 12:00:00")
+
+pb32608 <- traj.df %>%
+  filter(id == "pb_32608") %>%
+  filter(date > "2008-08-30 10:00:00" & date < "2008-10-15 07:00:00")
 
 # Divide into groups based on median fix rate
 

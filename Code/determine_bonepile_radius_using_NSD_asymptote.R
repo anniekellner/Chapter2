2e1e7d85e2cdf1ed87edc6ccba2cfc6a387bb0de
anniekellner <annie.kellner@colostate.edu>
2021-11-30 09:59:26 -0700

    all bears added for NSF asymptote analysis

--- Code/determine_bonepile_radius_using_NSD_asymptote.R
+++ Code/determine_bonepile_radius_using_NSD_asymptote.R
@@ -20,65 +20,62 @@ pb <- cbind(pb, st_coordinates(pb)) # separate coords from geometry columns into
 
 pbdf <- as.data.frame(pb)
 
-traj.pb<-as.ltraj(xy=pbdf[,c("X","Y")], date=pbdf$datetime, id=as.character(pbdf$id))
-traj.df <- ld(traj.pb)
 
 # --------------------  Create dataset that is 'asymptote' around bonepile    ---------- #
 
 # Bears with all points at bonepile (reference: bonepile_denning_info.xlsx)
 
-# Load akde table to get median fix rates
-
-akde <- readRDS('./Data/akde_df.Rds')
-
-bp_only <- traj.df %>%
+bp_only <- pbdf %>%
   filter(id == "pb_20525.2013" | id == "pb_20525.2014" | id == "pb_20586.2008" | id == "pb_32366.2014") %>%
 
 # Bears that travel to bonepile
 
-pb06810 <- traj.df %>%
+pb06810 <- pbdf %>%
   filter(id == "pb_06810.2008") %>%
   filter(date > "2008-09-16 18:00:37")
 
-pb20966 <- traj.df %>%
+pb20966 <- pbdf %>%
   filter(id == "pb_20966.2008") %>%
   filter(date > "2008-08-26 16:00:00")
 
-pb20492 <- traj.df %>%
+pb20492 <- pbdf %>%
   filter(id == "pb_20492.2008") %>%
   filter(date < "2008-10-16")
 
-pb20520 <- traj.df %>% 
+pb20520 <- pbdf %>% 
   filter(id == "pb_20520.2012") %>%
   filter(date > "2012-08-27 20:00:00")
 
-pb20735 <- traj.df %>%
+pb20735 <- pbdf %>%
   filter(id == "pb_20735.2009") %>%
   filter(date > "2009-08-09 22:00:00" & date < "2009-08-29 21:00:00" | date > "2009-09-16 01:00:00")
 
-pb20845 <- traj.df %>%
+pb20845 <- pbdf %>%
   filter(id == "pb_20845.2015") %>%
   filter(date > "2015-09-23 20:01:29" & date < "2015-10-02 15:00:09")
 
-pb21015 <- traj.df %>%
+pb21015 <- pbdf %>%
   filter(id == "pb_21015.2013") %>%
   filter(date > "2013-08-20 08:00:31" & date < "2013-09-27 04:00")
 
-pb21368 <- traj.df %>%
+pb21368 <- pbdf %>%
   filter(id == "pb_21368.2014") %>%
   filter(date > "2014-08-26 04:00:00") 
 
-pb32282 <- traj.df %>%
+pb32282 <- pbdf %>%
   filter(id == "pb_32282.2008") %>%
   filter(date > "2008-08-31 16:00:00")
 
-pb32366_2011 <- traj.df %>%
+pb32366_2011 <- pbdf %>%
   filter(id == "pb_32366.2011") %>%
   filter(date > "2011-08-30 12:00:00")
 
-pb32608 <- traj.df %>%
+all_bp <- pbdf %>%
   filter(id == "pb_32608") %>%
-  filter(date > "2008-08-30 10:00:00" & date < "2008-10-15 07:00:00")
+  filter(date > "2008-08-30 10:00:00" & date < "2008-10-15 07:00:00") %>%
+  bind_rows(bp_only, pb06810, pb20966, pb20492, pb20520, pb20735, pb20845, pb21015, pb21368, pb32282, pb32366_2011)
+
+
 
 # Divide into groups based on median fix rate
 

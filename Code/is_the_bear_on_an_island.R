07256205d947be551c05a213d8bed6fdce717337
anniekellner <annie.kellner@colostate.edu>
2022-01-10 13:21:06 -0700

    added 'on_island' to dataframe

--- Code/is_the_bear_on_an_island.R
+++ Code/is_the_bear_on_an_island.R
@@ -11,11 +11,13 @@ library(dplyr)
 
 rm(list = ls())
 
+arcgis <- 'C:/Users/akell/Documents/ArcGIS/Projects/Chapter2/Test' # to 'ground-truth' points in ArcGIS
+
 # Load data
 
 islands <- st_read('./Data/Spatial/Barrier_Islands/all_islands.shp') # shapefile with all islands
 
-all <- readRDS('./Data/ssf_1hr_sf.Rds') # sf object with all points - true and false
+all <- readRDS('./Data/Derived-data/bonepile_data_used_avail.Rds') # sf object with all points - true and false
 
 # Visual check - looks good
 
@@ -26,4 +28,9 @@ on_island = lengths(st_intersects(all, islands)) > 0 # creates a vector of wheth
 
 all <- cbind(all, on_island)
 
-saveRDS(all, file = './Data/ssf_1hr_sf_11122021.Rds') # save sf dataframe with column for on island
+# Send to ArcGIS to check
+
+#sample10 <- slice_sample(all, prop = .10, replace = FALSE) # randomly select 10% of observations
+#st_write(sample10, paste0(arcgis, "/", "islands_test.shp"))
+
+saveRDS(all, file = './Data/Derived-data/bonepile_data_used_avail.Rds') # save sf dataframe with column for on island

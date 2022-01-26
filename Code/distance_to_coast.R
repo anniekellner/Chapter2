790d121a49377e087685f9549ecaae98078e3486
anniekellner <annie.kellner@colostate.edu>
2022-01-26 13:52:24 -0700

    not sure

--- Code/distance_to_coast.R
+++ Code/distance_to_coast.R
@@ -3,45 +3,50 @@
 ###############################################
 
 # Coast = mainland coast
-# Distance: water --> land = -
-#           land --> water = +  
 
 library(dplyr)
 library(sf)
 library(tmap)
 library(tmaptools)
+library(amt)
 
 rm(list = ls())
-
-arcgis <- 'C:/Users/akell/Documents/ArcGIS/Projects/Chapter2/Test'
 
 # Load data
 
 coast <- st_read('./Data/Spatial/coastline/digitized_coastline.shp') # Used .shp of coastline as digitized from IFSAR images on GEE
 coast <- st_transform(coast, 3338)
 
-pts <- readRDS('./Data/Derived-data/bonepile_pts_used_avail.Rds')
-pts <- st_as_sf(pts, coords = c('x_', 'y_'), crs = 3338) # convert to sf
+pts <- readRDS('./Data/all_bonepile_points.Rds')
 
-# Plot
+mcp <- readRDS('./Data/Spatial/MCPs/mcp_bonepiles.Rds')
 
-#tmap_mode('view')
+# ------  Plot  ---------------------------------------------------------------- #
 
-#tm_shape(coast) + 
-  #tm_lines(col = "green") + 
-  #tm_shape(pts) + 
-  #tm_dots()
+tmap_mode('view')
 
-# Calculate distance for coast for used points
+tm_shape(coast) + 
+  tm_lines(col = "green") + 
+  tm_shape(pts) + 
+  tm_dots() + 
+  tm_shape(mcp) + 
+  tm_polygons(fill = id)
 
-dist_to_coast <- pts %>%
+
+
+
+  
+  
+
+# --------  Calculate distance to coast for used points ------------------------- #
+
+used <- pts %>%
+  select(id, geometry) %>%
   mutate(dist_to_coast = st_distance(., coast)) # will not work without . 
 
-# Check values in ArcGIS - look good 
+#saveRDS(used, './Data/used_bonepile.Rds')
 
-#sample10 <- slice_sample(dist_to_coast, prop = .10, replace = FALSE) # randomly select 10% of observations
-#st_write(sample10, paste0(arcgis, "/", "dist_to_coast.shp"))
 
-#saveRDS(pts, './Data/Derived-data/bonepile_data_used_avail.Rds')
 
-         
+
+

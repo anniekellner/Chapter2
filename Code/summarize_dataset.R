2505587fbdb1f483e6319bd4f454b4ae5af84a2c
anniekellner <annie.kellner@colostate.edu>
2022-01-26 13:52:30 -0700

    not sure

--- Code/summarize_dataset.R
+++ Code/summarize_dataset.R
@@ -3,10 +3,31 @@
 #####################################################
 
 library(dplyr)
+library(adehabitatLT)
+library(sf)
 
 rm(list = ls())
 
-akde <- readRDS('./Data/akde_df.Rds')
+# Summarize dataset for bonepile 
+
+bears <- readRDS('./Data/all_bonepile_points.Rds')
+pb <- cbind(bears, st_coordinates(bears)) # separate coords from geometry columns into X and Y columns
+
+pbdf <- as.data.frame(pb)
+
+traj.pb<-as.ltraj(xy=pbdf[,c("X","Y")], date=pbdf$datetime, id=as.character(pbdf$id))
+Summary.traj.pb <- summary(traj.pb)
+
+Summary.traj.pb$DaysTrack <-round(difftime(Summary.traj.pb$date.end,Summary.traj.pb$date.begin, units="days"),digits=1)
+Summary.traj.pb$Records <- Summary.traj.pb$nb.reloc-Summary.traj.pb$NAs
+Summary.traj.pb$PctComplete <- round((Summary.traj.pb$nb.reloc-Summary.traj.pb$NAs)/Summary.traj.pb$nb.reloc*100,digits=1)
+
+Summary.traj.pb %>%
+  dplyr::select(id, date.begin, date.end, DaysTrack) -> summary
+
+
+
+
 
 # Fix rates
 

ace303d4305fe6a6542833b380780a09f7b141e8
anniekellner <annie.kellner@colostate.edu>
2022-02-14 14:03:31 -0700

    added 0/1 island/not island to NB dataframe. Initial commit data for NB points

--- Code/Non_Bonepile_Points/is_the_bear_on_an_island.R
+++ Code/Non_Bonepile_Points/is_the_bear_on_an_island.R
@@ -10,10 +10,6 @@ rm(list= ls())
 # --------- Load data ------------------------------------------------------- #
 
 ua <- readRDS('./Data/Derived-data/non_bp_ua_sf.Rds')
-ua <- st_set_crs(ua, 3338)
-
-#saveRDS(ua, './Data/Derived-data/non_bp_ua_sf.Rds')
-
 
 islands <- st_read('./Data/Spatial/Barrier_Islands/all_islands.shp') # shapefile with all islands
 
@@ -24,7 +20,15 @@ plot(st_geometry(islands), col = "green")
 
 # ------  Analysis  ----------------------------------------------------- #
 
-ua <- ua %>%
-  mutate(on_island = st_intersection(., islands))
+on_island = lengths(st_intersects(ua, islands)) > 0 # creates a vector of whether or not point is on island
 
-on_island = lengths(st_intersects(all, islands)) > 0 # creates a vector of whether or not point is on island
+ua <- cbind(ua, on_island)
+
+# Send to ArcGIS to check
+
+arcgis <- 'C:/Users/akell/Documents/ArcGIS/Projects/Chapter2/Test' # to 'ground-truth' points in ArcGIS
+
+sample5 <- slice_sample(ua, prop = .5, replace = FALSE) # randomly select 5% of observations
+st_write(sample5, paste0(arcgis, "/", "islands_test_nb.shp")) # Looks good 2-14-22
+
+saveRDS(ua, './Data/Derived-data/NB_UA_data.Rds')

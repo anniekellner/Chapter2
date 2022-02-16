054d74b61d885d74b94b78a182c5985d66a8491f
anniekellner <annie.kellner@colostate.edu>
2022-02-16 14:20:40 -0700

    Extracted values for corr and bone and plotted using tmap to ground-truth

--- Code/get_veg_type.R
+++ Code/get_veg_type.R
@@ -8,26 +8,49 @@
 
 library(sf)
 library(dplyr)
+library(tmap)
+library(tmaptools)
 
 rm(list = ls())
 
 # ------------ Load data  -------------------------------- #
 
 land <- st_read('C:/Users/akell/Documents/PhD/Polar_Bears/Data/veg/aga_arctic_ak_geobotanical.shp') # veg shp
-land <- st_transform(land, 3338)
 
-land <- select(land, COMM:geometry)
+bb <- st_read('./Data/Spatial/Bounding_box/rectangle.shp')
 
 corr <- readRDS('./Data/Derived-data/corridor_data.Rds')
 bone <- readRDS('./Data/Derived-data/bonepile_data.Rds')
 
 
-# ----------  Extract Values for Corridor Points ------------------------------------------ #
+# Prep Data
+
+land <- st_transform(land, 3338)
+land <- select(land, COMM:geometry)
+
+bb <- st_bbox(bb)
+
+land <- st_crop(land, bb)
+
+
+# ----------  Extract Values ------------------------------------------ #
 
 corr <- st_intersection(corr, land)
 bone <- st_intersection(bone, land)
 
-unique(corr$COMM)
+# -----   Plot  -------------------------------------------------------- #
+
+tmap_mode('view')
+
+tm_shape(land) + 
+  tm_polygons(col = "COMM") + 
+  tm_layout(legend.outside = TRUE) + 
+  tm_shape(corr) + 
+  tm_symbols(size = 0.5,popup.vars = c("COMM"))
+
+# LOOKS GOOD!
+
+
 
 
 

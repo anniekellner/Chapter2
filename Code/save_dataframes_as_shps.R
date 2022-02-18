a7779d9ae74fbe75e9205049d815dae13877afc3
anniekellner <annie.kellner@colostate.edu>
2022-02-18 12:28:25 -0700

    initial commit

---
+++ Code/save_dataframes_as_shps.R
@@ -0,0 +1,22 @@
+#########################################################
+###   SAVE DATAFRAMES AS SHAPEFILES FOR USE WITH GEE  ###
+#########################################################
+
+library(sf)
+
+rm(list = ls())
+
+# ----  Read in data  -------------------------- #
+
+bone <- readRDS('./Data/Derived-data/bonepile_data.Rds')
+corr <- readRDS('./Data/Derived-data/corridor_data.Rds')
+
+# ----  Convert to sf object  ----------------- #
+
+bone <- st_as_sf(bone)
+corr <- st_as_sf(corr)
+
+# --  Write to shapefile  ----------------------- #
+
+st_write(bone, './Data/Derived-data/Shapefiles/bonepile_data.shp')
+st_write(corr, './Data/Derived-data/Shapefiles/corridor_data.shp')

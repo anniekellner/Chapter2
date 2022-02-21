33b806227ede5578800856b3543e6ae3a74c0aa0
anniekellner <annie.kellner@colostate.edu>
2022-02-21 15:34:14 -0700

    Do not need to separate scripts for bonepile and corridor points. Delete bonepile-specific script and rename

---
+++ Code/distance_to_coast.R
@@ -0,0 +1,63 @@
+########################################################
+###   DISTANCE TO MAINLAND COAST   #####################
+########################################################
+
+# Coast = mainland coast
+
+library(dplyr)
+library(sf)
+library(tmap)
+library(tmaptools)
+
+
+rm(list = ls())
+
+# ----- Load data ---------------------------------- #
+
+coast <- st_read('./Data/Spatial/coastline/digitized_coastline.shp') # Used .shp of coastline as digitized from IFSAR images on GEE
+coast <- st_transform(coast, 3338)
+
+bone <- readRDS('./Data/Derived-data/bonepile_data.Rds')
+corr <- readRDS('./Data/Derived-data/corridor_data.Rds')
+
+# Create sf objects 
+
+bone.sf <- st_as_sf(bone, crs = 3338)
+corr.sf <- st_as_sf(corr, crs = 3338)
+
+# --------  Calculate distance to coast for used points ------------------------- #
+
+corr.sf <- corr.sf %>%
+  #select(id, geometry) %>%
+  mutate(dist_to_coast = st_distance(., coast)) # will not work without . 
+
+# If in the water or on an island, make negative
+
+corr.sf$dist_to_coast <- ifelse(corr.sf$on_island == "TRUE" | corr.sf$in_water == 1, corr.sf$dist_to_coast*-1, corr.sf$dist_to_coast)
+
+# Check using distance tool in ArcGIS
+
+arcgis <- 'C:/Users/akell/Documents/ArcGIS/Projects/Chapter2/Test'
+
+sample1 <- slice_sample(corr.sf, prop = .01, replace = FALSE) # randomly select 1% of observations
+st_write(sample1, paste0(arcgis, "/", "dist_to_coast.shp"))
+
+# ------  Plot  ---------------------------------------------------------------- #
+
+tmap_mode('view')
+
+tm_shape(coast) + 
+  tm_lines(col = "green") + 
+  tm_shape(corr.sf) + 
+  tm_symbols(popup.vars = c('dist_to_coast'))
+
+
+
+
+
+
+
+
+
+
+

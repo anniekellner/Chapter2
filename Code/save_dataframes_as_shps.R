ae093bfffc75b1330a4f8f24bf7c066dafafce07
anniekellner <annie.kellner@colostate.edu>
2022-02-25 13:50:18 -0700

    export new bonepile df with 99 MCP to SHP for use with GEE

--- Code/save_dataframes_as_shps.R
+++ Code/save_dataframes_as_shps.R
@@ -11,18 +11,12 @@ rm(list = ls())
 bone <- readRDS('./Data/Derived-data/bonepile_data.Rds')
 corr <- readRDS('./Data/Derived-data/corridor_data.Rds')
 
-bone_pts <- readRDS('./Data/Derived-data/bonepile_pts_used_avail.Rds')
-
 # ----  Convert to sf object  ----------------- #
 
-bone <- st_as_sf(bone)
+bone <- st_as_sf(bone, coords = c('x_', 'y_'), crs = 3338)
 corr <- st_as_sf(corr)
-
-bone_pts <- st_as_sf(bone_pts, coords = c('x_', 'y_'), crs = 3338)
 
 # --  Write to shapefile  ----------------------- #
 
 st_write(bone, './Data/Derived-data/Shapefiles/bonepile_data.shp')
 st_write(corr, './Data/Derived-data/Shapefiles/corridor_data.shp')
-
-st_write(bone_pts, './Data/Derived-data/Shapefiles/bonepile_points.shp')

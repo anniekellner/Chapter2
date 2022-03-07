83bae62e26b47229204684f675f81675272afd8e
anniekellner <annie.kellner@colostate.edu>
2022-03-07 14:25:33 -0700

    initial commit

---
+++ Code/load_data.R
@@ -0,0 +1,7 @@
+#########################################################
+###   SCRIPT TO LOAD BEAR DATA  #########################
+#########################################################
+
+corr <- readRDS('./Data/Derived-data/corridor_data.Rds')
+
+bone <- readRDS('./Data/Derived-data/bonepile_data.Rds')

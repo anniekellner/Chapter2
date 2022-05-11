9970b5c28aee6961985d15d9870ad2b7d9161aab
anniekellner <annie.kellner@colostate.edu>
2022-05-11 14:04:58 -0600

    Re-did on_island with updated islands shapefile and 1.5 km buffer

--- Code/is_the_bear_on_an_island.R
+++ Code/is_the_bear_on_an_island.R
@@ -2,7 +2,7 @@
 ####      IS THE BEAR ON AN ISLAND?   ##########################
 ################################################################
 
-## Add a 5 km buffer to islands to account for GPS error and shoreline changes
+## Add a 1.5 km buffer to islands to account for GPS error and shoreline changes
 
 library(dplyr)
 library(sf)
@@ -13,20 +13,23 @@ rm(list= ls())
 
 # --------- Load data ------------------------------------------------------- #
 
-corr <- readRDS('./Data/Derived-data/corridor_data.Rds') # used + available
-corr.sf <- st_as_sf(corr, crs = 3338)
+corr <- readRDS('./Data/Derived-data/corridor_data.Rds') # reads in as sf
 
 bone <- readRDS('./Data/Derived-data/bonepile_data.Rds') # reads in as sf
 
 #bone_pts <- readRDS('./Data/all_bonepile_points.Rds') # used only - bonepile
 #corr_pts <- readRDS('./Data/all_non_bonepile_pts.Rds') # used only - corridor
 
-islands <- st_read('./Data/Spatial/Barrier_Islands/all_islands.shp') # shapefile with all islands
+islands <- st_read('./Data/Spatial/Barrier_Islands/all_islands_v2.shp') # shapefile with all islands, updated 5/11/22
 
 # ------- Plot  ---------------------------------------------------------- #
 
-plot(st_geometry(corr.sf))
-plot(st_geometry(islands), col = "green", add = TRUE)
+tmap_mode('view')
+
+tm_shape(islands) + 
+  tm_polygons(col = "green") + 
+  tm_shape(corr) + 
+  tm_symbols(alpha = 0.25)
 
 # ------  Add buffer to islands ------------------------------------ #
 
@@ -53,21 +56,48 @@ tm_shape(buff) +
 
 # Save because it takes time to create these buffers
 
-#st_write(buff, './Data/Spatial/islands_w_1500m_buffer.shp')
+st_write(buff, './Data/Spatial/islands_w_1500m_buffer.shp')
 
 
 # ------  Analysis  ----------------------------------------------------- #
 
-on_island = lengths(st_intersects(corr.sf, buff)) > 0 # creates a vector of whether or not point is on island
-on_island = lengths(st_intersects(bone, buff)) > 0
+## Corr pts
 
-ua_bone <- cbind(bone, on_island)
+on_island_corr = lengths(st_intersects(corr, buff)) > 0 # creates a vector of whether or not point is on island
 
-# Send to ArcGIS to check
+corr <- corr %>%
+  select(-on_island) %>% 
+  cbind(on_island_corr) %>%
+  rename(on_island = on_island_corr)
 
-arcgis <- 'C:/Users/akell/Documents/ArcGIS/Projects/Chapter2/Test' # to 'ground-truth' points in ArcGIS
+# Check  
 
-sample5 <- slice_sample(ua, prop = .5, replace = FALSE) # randomly select 5% of observations
-st_write(sample5, paste0(arcgis, "/", "islands_test_nb.shp")) # Looks good 2-14-22
+sample5 <- slice_sample(corr, prop = .05, replace = FALSE) # randomly select 5% of observations
 
-saveRDS(ua_bone, './Data/Derived-data/bonepile_data.Rds')
+tm_shape(buff) + 
+  tm_polygons(col = "green") + 
+  tm_shape(sample5) + 
+  tm_symbols(size = 0.25, popup.vars = "on_island") # looks good
+
+## Bone pts
+  
+on_island_bone = lengths(st_intersects(bone, buff)) > 0
+
+bone <- bone %>%
+  select(-on_island) %>%
+  cbind(on_island_bone) %>%
+  rename(on_island = on_island_bone)
+
+# Check
+
+sample5 <- slice_sample(bone, prop = .05, replace = FALSE) # randomly select 5% of observations
+
+tm_shape(buff) + 
+  tm_polygons(col = "green") + 
+  tm_shape(sample5) + 
+  tm_symbols(size = 0.25, popup.vars = "on_island") # looks good
+
+# ----------- Save data ------------------------------ #
+
+saveRDS(bone,'./Data/Derived-data/bonepile_data.Rds')
+saveRDS(corr, './Data/Derived-data/corridor_data.Rds')

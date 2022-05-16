7f7655acf8af464846bdd04a4ace90cdcdc4e59d
anniekellner <annie.kellner@colostate.edu>
2022-05-16 08:23:57 -0600

    Re-calculated distance to coast with NA/s for water and island points. Checking v. small sample using distance tool in ArcGIS

--- Code/distance_to_coast.R
+++ Code/distance_to_coast.R
@@ -3,7 +3,8 @@
 ########################################################
 
 ## Only calculating distance to coast for mainland points (decided at meeting with GW 4/13/22)
-## This metric is really only relevant for mainland points, anyway, and the positive/negative denotation meses with coefficients
+## This metric is really only relevant for mainland points
+## The positive/negative denotation messes with coefficients
 
 # Coast = mainland coast. Digitized using combo of shapefile and IfSAR.
 
@@ -23,30 +24,33 @@ coast <- st_transform(coast, 3338)
 bone <- readRDS('./Data/Derived-data/bonepile_data.Rds')
 corr <- readRDS('./Data/Derived-data/corridor_data.Rds')
 
-# Create sf objects 
 
-bone.sf <- st_as_sf(bone, coords = c('x_', 'y_'), crs = 3338)
-corr.sf <- st_as_sf(corr, crs = 3338)
+# --------  Calculate distance to coast  ------------------------- #
 
-
-
-
-# --------  Calculate distance to coast for used points ------------------------- #
-
-corr.sf <- corr.sf %>%
+corr <- corr %>%
   mutate(dist_to_coast = st_distance(., coast)) # will not work without . 
-
+  
 bone.sf <- bone.sf %>%
   mutate(dist_to_coast = st_distance(., coast))
 
+# --------  Replace points in water & on islands with NA ---------- #
+
+corr2 <- corr %>%
+  mutate(dist_to_coast = ifelse(in_water == 1 | on_island == TRUE, NA_character_, dist_to_coast))
+
+bone2 <- bone %>%
+  mutate(dist_to_coast = ifelse(in_water == 1 | on_island == TRUE, NA_character_, dist_to_coast))
+
 # Check using distance tool in ArcGIS
+# Looks good
 
 arcgis <- 'C:/Users/akell/Documents/ArcGIS/Projects/Chapter2/Test'
 
-sample1 <- slice_sample(corr.sf, prop = .01, replace = FALSE) # randomly select 1% of observations
-st_write(sample1, paste0(arcgis, "/", "dist_to_coast.shp"), append = FALSE)
+sample_corr <- slice_sample(corr2, prop = .01, replace = FALSE) # randomly select 1% of observations
+st_write(sample_corr, paste0(arcgis, "/", "dist_to_coast_test_corr.shp"), append = FALSE)
 
-bone_sample1 <- slice_sample(bone.sf, prop = .01, replace = FALSE)
+sample_bone <- slice_sample(bone2, prop = .01, replace = FALSE)
+st_write(sample_bone, paste0(arcgis, "/", "dist_to_coast_test_bone.shp"), append = FALSE)
 
 # ------  Plot  ---------------------------------------------------------------- #
 
@@ -59,8 +63,8 @@ tm_shape(coast) +
 
 # ----- Save  ------------------------------------------------------------------ #
 
-saveRDS(bone.sf, "./Data/Derived-data/bonepile_data.Rds")
-
+saveRDS(bone2, "./Data/Derived-data/bonepile_data.Rds")
+saveRDS(corr2, "./Data/Derived-data/corridor_data.Rds")
 
 
 

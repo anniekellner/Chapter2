bac14c1ce320129da7c859072f0a74a827ea46ae
anniekellner <annie.kellner@colostate.edu>
2022-05-25 11:48:19 -0600

    starting to create shapefiles from all files in a folder

--- Code/energy_distance_to_infrastructure.R
+++ Code/energy_distance_to_infrastructure.R
@@ -4,6 +4,12 @@
 
 # Combine all energy infrastructure into a single entity and calculate distance to it
 
+library(sf)
+library(dplyr)
+
 rm(list = ls())
 
-endicott <- 
+# Read in all .shp files from a directory
+
+cp <- list.files(path = './Data/Spatial/Industry_GIS/CP_Infrastructure', pattern = "[.]shp$", full.names = TRUE)
+fac <- list.files(path = './Data/Spatial/Industry_GIS/Facilities', pattern = "[.]shp$", full.names = TRUE)

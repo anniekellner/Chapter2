f6777456512d9be51735a0d5d1ee5c3c9a5c7c12
anniekellner <annie.kellner@colostate.edu>
2022-05-25 08:24:55 -0600

    Initial commit

---
+++ Code/energy_distance_to_infrastructure.R
@@ -0,0 +1,9 @@
+################################################
+#####   ENERGY: DISTANCE TO INFRASTRUCTURE  ####
+################################################
+
+# Combine all energy infrastructure into a single entity and calculate distance to it
+
+rm(list = ls())
+
+endicott <- 

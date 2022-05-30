e66475ff7ee3ea3d748892ca640fdf06c12a7867
anniekellner <annie.kellner@colostate.edu>
2022-05-30 10:16:24 -0600

    combined all cp layers into one shp

--- Code/energy_distance_to_infrastructure.R
+++ Code/energy_distance_to_infrastructure.R
@@ -12,4 +12,16 @@ rm(list = ls())
 # Read in all .shp files from a directory
 
 cp <- list.files(path = './Data/Spatial/Industry_GIS/CP_Infrastructure', pattern = "[.]shp$", full.names = TRUE)
+
+cp_shp <- list()
+
+for(i in 1:length(cp)){
+ shp = st_read(cp[i])
+ shp = st_transform(shp, 3338)
+cp_shp[[i]] = shp
+}
+
+cp2 <- bind_rows(cp_shp)
+plot(st_geometry(cp2))
+
 fac <- list.files(path = './Data/Spatial/Industry_GIS/Facilities', pattern = "[.]shp$", full.names = TRUE)

46fe45510be9da6a82af0fc07d85a2e414a7eb55
anniekellner <annie.kellner@colostate.edu>
2022-06-01 15:30:31 -0600

    Created a grid with road density. Need to figure out how large the cells are and map polar bear locations onto grid

--- Code/road_density.R
+++ Code/road_density.R
@@ -2,14 +2,46 @@
 #########   ROAD DENSITY - RESIDENTIAL  ############
 ####################################################
 
+# Tiger roads were obtained from TIGER US Census Roads from the 2016 release. 
+# Acquired via Google Earth Engine 2/17/22.
+# I am assuming these are municipal/residential roads
+
+# https://stackoverflow.com/questions/69035297/calculating-road-density-raster-from-road-shapefile
+
 library(sf)
 library(dplyr)
-
+library(terra)
 
 rm(list = ls())
 
 # ------- DATA  ------------------------ #
 
-roads <- st_read('C:/Users/akell/OneDrive - Colostate/PhD/Chapter2/Data/Roads/TIGER-roads/tiger-roads.shp')
-roads <- st_transform(roads, 3338)
+r <- st_read('./Data/Spatial/Roads/tiger-roads.shp')
+r <- st_transform(r, 3338)
 
+corr <- readRDS('./Data/Derived-data/corridor_data.Rds')
+bone <- readRDS('./Data/Derived-data/bonepile_data.Rds')
+
+# --- ANALYSIS  ----------------------- #
+
+v <- vect(r)
+roads <- as.lines(v)
+rs <- rast(v)
+#res(rs) <- 1000 # 1 km x 1 km 
+
+values(rs) <- 1:ncell(rs)
+names(rs) <- "rast"   
+rsp <- as.polygons(rs) # raster to polygons
+
+rp <- intersect(roads, rsp)
+
+rp$length <- perim(rp) / 1000 #km
+x <- tapply(rp$length, rp$rast, sum) # calculates the length of road in each cell 
+
+r <- rast(rs)
+r[as.integer(names(x))] <- as.vector(x)
+
+# ---- PLOT ------------------------ #
+
+plot(r)
+lines(roads)

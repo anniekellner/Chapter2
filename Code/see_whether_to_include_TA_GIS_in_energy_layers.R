304f32dedef167a69b9d55ed9b4e81af9f3e69ac
anniekellner <annie.kellner@colostate.edu>
2022-08-16 10:03:49 -0600

    finished pipes - duplicate

--- Code/see_whether_to_include_TA_GIS_in_energy_layers.R
+++ Code/see_whether_to_include_TA_GIS_in_energy_layers.R
@@ -4,6 +4,8 @@
 
 library(sf)
 library(dplyr)
+library(tmap)
+library(tmaptools)
 
 rm(list = ls())
 
@@ -37,7 +39,7 @@ pipes <- st_read('C:/Users/akell/OneDrive - Colostate/Documents/ArcGIS/GIS from 
   select(geometry) %>%
   mutate(layer = "pipes")
 
-prop_fac <- st_read('C:/Users/akell/OneDrive - Colostate/Documents/ArcGIS/GIS from Todd/Industry GIS/OIL/PROP_FAC.shp') %>%
+prop_fac <- st_read('C:/Users/akell/OneDrive - Colostate/Documents/ArcGIS/GIS from Todd/Industry GIS/OIL/PROP_FAC.shp') %>% # Looks like facilities and pipes?
   st_transform(3338) %>%
   select(geometry) %>%
   mutate(layer = "PROP_FAC")
@@ -54,6 +56,9 @@ tiger_rds <- st_read('C:/Users/akell/OneDrive - Colostate/Documents/ArcGIS/GIS f
 
 # ------- See whether these shapefiles are duplicates of CP and Hilcorp ----------------------------------------------- #
 
+## PIPES
+# DUPLICATE!! Do not need to use pipes shp
+
 hil_pipes <- st_read('./Data/Spatial/Industry_GIS/Hilcorp/Pipes_Hilcorp.shp') %>%
   select(geometry) %>%
   st_transform(3338) %>%
@@ -64,8 +69,54 @@ cp_pipes <- st_read('./Data/Spatial/Industry_GIS/CP_Infrastructure/Kuparuk_Pipel
   st_transform(3338) %>%
   mutate(company = "CP")
 
-ind_pipes <- st_union(hil_pipes, cp_pipes)
+ind_pipes <- bind_rows(hil_pipes, cp_pipes)
 
-inter_pipes <- st_intersection(ind_pipes, pipes)
+# Plot 
 
-plot(st_geometry(inter_pipes))
+tmap_mode('view')
+
+tm_shape(ind_pipes) + 
+  tm_lines(col = "gray", lwd = 2) +
+  tm_shape(pipes) + 
+  tm_lines(col = "gray", lwd = 1)
+
+## FACILITIES
+
+kup_pads <- st_read('./Data/Spatial/Industry_GIS/CP_Infrastructure/Kuparuk_Gravel_Pads.shp') %>%
+  select(geometry) %>%
+  st_transform(3338) %>%
+  mutate(company = "CP")
+  
+hilfac <- st_read('./Data/Spatial/Industry_GIS/Hilcorp/Facilities_Hilcorp.shp') %>%
+  select(geometry) %>%
+  st_transform(3338) %>%
+  mutate(company = "Hilcorp")
+
+ind_fac <- bind_rows(kup_pads, hilfac)
+
+# Troubleshoot invalid geometries - https://r-spatial.org/r/2017/03/19/invalid.html
+
+any(is.na(st_dimension(ind_fac))) # empty geometries
+any(is.na(st_is_valid(ind_fac))) # corrupt geometries
+any(na.omit(st_is_valid(ind_fac)) == FALSE) # invalid geometries
+st_is_valid(ind_fac, reason = TRUE) # Ring Self-intersection (1x)
+
+ind_fac <- st_make_valid(ind_fac) # Works - fixed!
+
+oldfac <- bind_rows(fac, fac2, prop_fac)
+
+any(is.na(st_dimension(oldfac))) # empty geometries
+any(is.na(st_is_valid(oldfac))) # corrupt geometries
+any(na.omit(st_is_valid(oldfac)) == FALSE) # invalid geometries
+invalid <- st_is_valid(oldfac, reason = TRUE); unique(invalid) # "too few points in geometry component" (9x)
+
+oldfac <- st_make_valid(oldfac) # Works
+
+# Plot
+
+plot(st_geometry(oldfac))
+
+tm_shape(ind_fac) + 
+  tm_polygons(col = "blue") + 
+  tm_shape(oldfac) + 
+  tm_polygons(col = "red")

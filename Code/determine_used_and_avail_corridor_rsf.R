7c70ec72647a32fa8f74f19c5404060bf071de62
anniekellner <annie.kellner@colostate.edu>
2022-09-08 10:17:25 -0600

    Remove distance to coast and other summary stat calculations for water points (corridor bears). Looking to use MCP's instead

--- Code/determine_used_and_avail_corridor_rsf.R
+++ Code/determine_used_and_avail_corridor_rsf.R
@@ -2,83 +2,27 @@
 #########   DETERMINE USED AND AVAILABLE POINTS FOR CORRIDOR RSF ###########
 ############################################################################
 
-library(sf)
+library(amt)
 library(dplyr)
+library(sf)
+library(tmap)
+library(tmaptools)
 
 rm(list = ls())
 
-# ---------- LOAD DATA  ------------------------ #
+source('./Code/MyFunctions.R')
 
-corr <- readRDS('./Data/Derived-data/corridor_data.Rds')
+# -------------   LOAD AND FORMAT DATA   ------------------------ #
 
-corr <- corr %>%
-  st_as_sf() %>%
-  st_set_crs(3338) 
+cor <- readRDS('./Data/Derived-data/corridor_data.Rds')
 
-corr <- filter(corr, case_ == "TRUE") # only include used points
+cor <- filter(cor, case_ == "TRUE") 
 
-corr <- as_tibble(corr) # see what class the values are
+## Make track with amt
 
-corr$dist_to_coast <- as.numeric(corr$dist_to_coast)
+track <- make_track(cor, x1_, y1_, crs = 3338, id = id) # makes one big track with all animals
+trk <- track %>% nest(data = -"id") # creates dataframes for each individual
 
-## Values for mainland points (because I only have values for mainland points in corr dataframe)
-
-max(corr$dist_to_coast, na.rm = TRUE) # 27.2 km
-mean(corr$dist_to_coast, na.rm = TRUE) # 2 km  
-sd(corr$dist_to_coast, na.rm = TRUE)
-
-# --------  Calculate distance to coast for water points ------------------------- #
-
-corr <- corr %>%
-  mutate(dist_to_coast = st_distance(., coast)) # will not work without . 
-
-water <- corr %>%
-  mutate(dist_to_coast = ifelse(in_water == 0, NA_character_, dist_to_coast))
-
-water$dist_to_coast <- as.numeric(water$dist_to_coast)
-
-## Values for water points
-
-max(water$dist_to_coast, na.rm = TRUE) # 58 km
-mean(water$dist_to_coast, na.rm = TRUE) # 11 km
-sd(water$dist_to_coast, na.rm = TRUE) # 16 km 
-
-# --------  Plot  --------------------------------------------- #
-
-water <- filter(corr, in_water == 1)
-sample_water <- slice_sample(water, prop = .01, replace = FALSE) # randomly select 1% of observations
-
-## Plot to check that distance fxn worked correctly
-
-tmap_mode('view')
-
-tm_shape(coast) + # Looks OK but did not verify using Measure tool in ArcGIS
-  tm_lines(col = "green") + 
-  tm_shape(sample_water) + 
-  tm_symbols(col = popup.vars = 'dist_to_coast')
-
-## Plot to show George and Stewart what location distribution looks like
-
-main <- filter(corr, in_water == 0 & on_island == 0)
-
-tmap_mode('view')
-
-tm_shape(coast) + 
-  tm_lines(col = "#875AC1") +
-  tm_shape(water) + 
-  tm_symbols(col = "#2467C2") + 
-  tm_shape(main) + 
-  tm_symbols(col = "#49491A")
-
-# ------ OTHER NUMBERS  ------------------------------- #
-  
-# How many points are in water vs. mainland vs. island:
-
-sum(corr$in_water) # 502 (18%)
-sum(corr$on_island) # 1510 (54%)
-# mainland = 778 (28%)
-
-unique(corr$id)
   
   
   

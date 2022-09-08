4216499624b1a92166ef8990995aa02f45d32369
anniekellner <annie.kellner@colostate.edu>
2022-09-08 10:57:28 -0600

    sampled available points and created plots with used v. available points and MCP's. Plots need more info but this is a start

--- Code/determine_used_and_avail_corridor_rsf.R
+++ Code/determine_used_and_avail_corridor_rsf.R
@@ -7,6 +7,7 @@ library(dplyr)
 library(sf)
 library(tmap)
 library(tmaptools)
+library(ggplot2)
 
 rm(list = ls())
 
@@ -23,6 +24,36 @@ cor <- filter(cor, case_ == "TRUE")
 track <- make_track(cor, x1_, y1_, crs = 3338, id = id) # makes one big track with all animals
 trk <- track %>% nest(data = -"id") # creates dataframes for each individual
 
+# Create random points using 99 MCP for each individual
   
-  
-  
+rsf <- data.frame(case_ = logical(), x_ = double(), y_ = double(), id = character())
+ids <- unique(cor$id)  
+
+for(i in 1:length(ids)){
+  bear_trk = trk$data[[i]]
+  hr = hr_mcp(bear_trk, levels = 0.99) # tried 95 first, but 99 looks better (just like BP bears)
+  rsf_pts = random_points(hr, n = 20*nrow(bear_trk), presence = bear_trk)
+  rsf_pts$id = ids[i]
+  rsf = rbind(rsf, rsf_pts)
+}
+
+# Plot random vs. matched points
+
+ggplot(rsf, aes(x_, y_, color = case_)) +
+  geom_point() +
+  facet_wrap(~id, scales = "free")
+
+# Plot MCP's
+
+cor.sf <- cor %>%
+  st_as_sf(., coords = c('x1_', 'y1_'), crs = 3338) %>%
+  dplyr::select(id, geometry)
+
+cor.sp <- as_Spatial(cor.sf)
+
+mcps <- mcp(cor.sp, percent = 99)
+
+mcp.sf <- st_as_sf(mcps) # because I hate sp
+
+ggplot(data = mcp.sf) +
+  geom_sf(mapping = aes(fill = id, alpha = 0.5))

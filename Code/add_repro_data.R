db15c513ba3310866076948be9834967ac20cbfa
anniekellner <annie.kellner@colostate.edu>
2022-09-19 16:32:39 -0600

    Have repro data for all bears except subadults. Some of the repro data is extrapolated. I should find out which are extrapolated and which are not

--- Code/add_repro_data.R
+++ Code/add_repro_data.R
@@ -9,7 +9,12 @@ rm(list = ls())
 # ------ Load and prep data  -------------- #
 
 pb <- readRDS('./Data/Derived-data/DFs/bears_ch2_091922.Rds')
-repro <- readRDS('./Data/Derived-data/DFs/repro.Rds') # From Chapter 1
+ch1 <- readRDS('C:/Users/akell/OneDrive - Colostate/PhD/Polar_Bears/Repos/ch1_landing/data/derived-data/all.Rds') # Different dataset from Ch1
+
+repro <- ch1 %>% 
+  select(animal, year, id, repro) %>%
+  group_by(id) %>%
+  slice_head()
 
 ids <- unique(pb$id)
 
@@ -25,5 +30,15 @@ pb2 <- pb %>%
 pb2 %>%
   group_by(id) %>%
   slice_head() %>%
-  select(id, repro, age) %>%
+  select(id, animal, repro, age) %>%
   print(n = 21)
+
+# ---   Look into missing data for 20418 ---------------- #
+
+ch1 %>% 
+  select(animal, year, id, repro) %>%
+  group_by(animal) %>%
+  slice_head() %>%
+  filter(animal == "pb_20418")
+
+# In 2004, pb_20418 had a coy. Missing data is for 2005. Possibly had a yearling. 

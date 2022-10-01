ceef1f70dfe2d84cba5af1470e10c1da8d7ed55a
anniekellner <annie.kellner@colostate.edu>
2022-10-01 12:22:11 -0600

    csv had a space after Cross name that was fucking up join

--- Code/explore_timing_and_harvest_dates.R
+++ Code/explore_timing_and_harvest_dates.R
@@ -13,24 +13,41 @@ rm(list = ls())
 
 bpt <- readRDS('./Data/Derived-data/DFs/Space_Use_Summaries/time_at_bonepile.Rds')
 
-bpt$year <- year(bpt$start)
-years <- unique(bpt$year)
+bpt$Year <- year(bpt$start)
+years <- unique(bpt$Year)
 
 bpt <- bpt %>%
-  select(id, start, end, year) %>%
-  mutate(bonepile = if_else(
+  select(id, start, end, Year) %>%
+  mutate(Bonepile = if_else(
     id == "pb_20492.2008" | id == "pb_20520.2012" | id == "pb_20735.2009" | id == "pb_20966.2008" | 
       id == "pb_20982.2008" | id == "pb_32282.2008" | id == "pb_32366.2011" | id == "pb_32608.2008",
-    "Kaktovik", "Cross"
-  )) %>%
+    "Kaktovik", "Cross")) %>%
   mutate(t_interval = interval(start = start, end = end, tzone = 'US/Alaska'))
 
 ## Harvest
 
 harvest <- read.csv('./Data/Bonepile_Dates.csv')
 
+harvest$Dates <- mdy(harvest$Dates)
+harvest$Year <- year(harvest$Dates)
+
+h <- harvest %>%
+  filter(Year %in% years) %>%
+  filter(!(Bonepile == "Barrow")) %>%
+  mutate(Bonepile = recode(Bonepile, "Cross " = "Cross")) # Because there was a space in csv file and wouldn't join correctly
+
 # ------------------   JOIN -------------------------------------------------- #
 
+bpt2 <- bpt %>%
+  full_join(h, by = "Bonepile") %>%
+  dplyr::rename(c(BP_date = Dates, BP_year = Year.y, Bear_year = Year.x)) %>%
+  arrange(id, start) %>%
+  as_tibble() %>%
+  print(width = Inf)
+  
+
+
+head(bpt)
 
 
 

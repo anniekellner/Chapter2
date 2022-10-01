1fd7bc2687ddf8f33b5f11c95888d6fcfaf1dbdd
anniekellner <annie.kellner@colostate.edu>
2022-10-01 11:37:53 -0600

    prep data

--- Code/explore_timing_and_harvest_dates.R
+++ Code/explore_timing_and_harvest_dates.R
@@ -2,25 +2,37 @@
 #####     IS THERE SOCIAL STRATIFICAION SURROUNDING THE HARVEST   #####################
 #######################################################################################
 
+library(lubridate)
+library(dplyr)
+
 rm(list = ls())
 
-# --------------------  LOAD DATA ------------------------------------------------ #
+# --------------------  LOAD AND PREP DATA ------------------------------------------------ #
 
-# Harvest
+## Bonepile
+
+bpt <- readRDS('./Data/Derived-data/DFs/Space_Use_Summaries/time_at_bonepile.Rds')
+
+bpt$year <- year(bpt$start)
+years <- unique(bpt$year)
+
+bpt <- bpt %>%
+  select(id, start, end, year) %>%
+  mutate(bonepile = if_else(
+    id == "pb_20492.2008" | id == "pb_20520.2012" | id == "pb_20735.2009" | id == "pb_20966.2008" | 
+      id == "pb_20982.2008" | id == "pb_32282.2008" | id == "pb_32366.2011" | id == "pb_32608.2008",
+    "Kaktovik", "Cross"
+  )) %>%
+  mutate(t_interval = interval(start = start, end = end, tzone = 'US/Alaska'))
+
+## Harvest
 
 harvest <- read.csv('./Data/Bonepile_Dates.csv')
 
-# Bonepile
-bpt <- readRDS('./Data/Derived-data/DFs/Space_Use_Summaries/time_at_bonepile.Rds')
+# ------------------   JOIN -------------------------------------------------- #
 
-## To get summary of time spent, have to add 20735's stints together and remove second entry
 
-bpt$time_spent <- as.numeric(bpt$time_spent)
-bpt[17,4] <- sum(bpt[17,4] + bpt[18,4])
 
-bpt[-18,] -> bpt
 
-bpt2 <- bpt %>%
-  select(id, time_spent) %>%
-  mutate(where = "Bonepile") %>%
-  dplyr::rename(time = time_spent) # because also in Plyr so gives error
+
+

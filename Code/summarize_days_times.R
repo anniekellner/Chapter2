65bd9305f4a5047d65dee86c64077cca83e17281
anniekellner <annie.kellner@colostate.edu>
2022-09-30 19:11:35 -0600

    Reserve 'landfall' column for bears that were collared at sea. 'Study.start' column is for all bears

--- Code/summarize_days_times.R
+++ Code/summarize_days_times.R
@@ -40,33 +40,32 @@ Summary.traj.pb %>%
 
 # ----- SUMMARIZE --------------------------------- #
 
-# Days tracked
+## Days tracked
 
 summary$DaysTrack <- as.numeric(summary$DaysTrack)
 
 summary(summary$DaysTrack)
 sd(summary$DaysTrack)
 
-# Deal with dates and times
+## Deal with dates and times
 
 # Landfall
 
-l <- bears %>% # 'landfall' is misnamed for bears that were collared on land - change
-  filter(landfall == 1) 
+# Remove 'landfall' designation for 2008 bears that were collared on land
 
-ch2 <- unique(l$id)  
-  
-end.swim <- all.v2 %>%
-  dplyr::filter(end.swim == 1)
+bears$study.start <- bears$landfall # Change what is now 'landfall' to study.start
 
-end <- unique(end.swim$id)
+# Reserve 'landfall' for bears that were collared at sea and came ashore
 
-dplyr::setdiff(ch2, end) # look at 20525.2014 and 
+b2008 <- filter(bears, year == 2008)
+b2008 <- unique(b2008$id)
+b2008 <- b2008[-2] # remove pb_20333.2008 because this bear was collared on ice
 
-b11 <- all.v2 %>%
-  dplyr::filter(id == "pb_21237.2011") 
+bears2 <- bears %>%
+  mutate(landfall = if_else(id %in% b2008, 0, landfall))
 
-b14l <- l %>%
+saveRDS(bears2, './Data/derived-data/DFs/bears_ch2_093022.Rds') # save with 'landfall' and 'study.start' columns changed
+
 
 
 s <- summary %>%

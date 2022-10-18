9f2b9aee8d41dbd84ef3a12150f3ba77a605338d
anniekellner <annie.kellner@colostate.edu>
2022-10-18 15:07:22 -0600

    bring in TA data and create column to match id

--- Code/denning_dates_and_locations.R
+++ Code/denning_dates_and_locations.R
@@ -2,3 +2,21 @@
 ########    DENNING DATES AND LOCATIONS   ################
 ##########################################################
 
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(lubridate)
+
+rm(list = ls())
+
+
+# --- LOAD DATA ------------- #
+
+den <- read.csv('./Data/Denning_locs_dates.csv')
+den$entrance <- mdy(den$entrance)
+
+den$year <- year(den$entrance)
+
+den <- den %>% # add column for id
+  mutate(X = str_trim(X, side = "right")) %>% # remove whitespace
+  unite("id", X, year, sep = '.')

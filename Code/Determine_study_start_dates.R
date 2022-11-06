3abb366e4125c3fe7563698d4035e2c30a4b741d
anniekellner <annie.kellner@colostate.edu>
2022-11-06 14:05:29 -0700

    replaced study.start in ch2 df with new start dates reflecting 5-day post-capture period

--- Code/Determine_study_start_dates.R
+++ Code/Determine_study_start_dates.R
@@ -7,15 +7,19 @@
 
 library(dplyr)
 library(lubridate)
+library(sf)
+library(tidyr)
 
 rm(list = ls())
 
 # ----- LOAD DATA ---------------------- #
 
-b <- readRDS('./Data/Derived-data/DFs/bears_ch2_093022.Rds') # all bears
+all <- readRDS('./Data/Derived-data/DFs/all.Rds')
+
+b <- readRDS('./Data/Derived-data/DFs/bears_ch2_093022.Rds') # ch 2 data
 
 s <- b %>% # landfall OR start date
-  select(id, landfall, study.start, date) %>%
+  select(id, landfall, study.start, date, datetime) %>%
   filter(landfall == 1 | study.start == 1)
 
 # --------  Bears that made landfall ---------------------------- #
@@ -27,9 +31,10 @@ lf$date <- mdy(lf$date)
 lf$ordinal <- yday(lf$date)
 summary(lf$ordinal)
 
-lf %>% arrange()
+lf %>% arrange(ordinal)
 
-lf$start_date <- lf$landfall
+lf <- rename(lf, start_date = date)
+
 
 # ------------ Bears that were collared on land ---------------------- #
 
@@ -37,16 +42,58 @@ c <- s %>%
   filter(landfall == 0 & study.start == 1) # 8 bears collared on land (all in 2008)
 
 c$date <- mdy(c$date) 
-c$ordinal <- yday(c$date)
 
-# Add five days to account for capture effects
+c$start_date <- c$date + ddays(5) # Add five days to account for capture effects
 
-c$start_date <- c$date + ddays(5)
+c$ordinal <- yday(c$start_date)
 
-# -------- Combine into 1 start date df ----------------- #
+summary(c$ordinal)
 
-s <- rbind(lf, c)
+c %>% arrange(ordinal)
 
-# FINISH TONIGHT
+c2 <- select(c, id, start_date)
 
+cids <- unique(c2$id)
 
+# --------------- REJOIN TO DATABASE  ---------------------------------- #
+
+b2 <- b %>%
+  st_drop_geometry() %>%
+  filter(id %in% cids)
+
+b3 <- b2 %>%
+  left_join(c2) %>%
+  select(-study.start)
+
+b4 <- b3 %>%
+  group_by(id, ymd) %>%
+  slice_head() %>%
+  mutate(study.start = 
+           if_else(ymd == start_date, 1, 0)) 
+
+b <- select(b, -study.start) # most recent ch2 df
+
+ch2 <- b %>%
+  left_join(b4) %>%
+  select(-start_date)
+
+ch2 <- ch2 %>%
+  mutate(study.start = 
+           if_else(landfall == 1, 1, study.start)) 
+
+ch2 <- ch2 %>%
+  replace_na(list(study.start = 0))
+
+ch2 %>% filter(study.start == 1) # looks good 
+  
+
+# -------- Combine into 1 start date df for summary stats ----------------- #
+
+lf2 <- select(lf, id, start_date, ordinal)
+c2 <- select(c, id, start_date, ordinal)
+
+sd <- rbind(lf2, c2)
+
+summary(sd)
+
+sd %>% arrange(ordinal)

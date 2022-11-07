372f4e5526cbe80cc5fc43fc7fe9fd099d9cc072
anniekellner <annie.kellner@colostate.edu>
2022-11-06 20:56:44 -0700

    save den locations and den entrance dates into all df. Resave all df and add previous to 'old' folder

--- Code/add_denning_start_dates_to_df.R
+++ Code/add_denning_start_dates_to_df.R
@@ -6,7 +6,9 @@
 
 library(dplyr)
 library(sf)
-library(zoo)
+library(lubridate)
+library(tidyr)
+library(stringr)
 
 
 rm(list = ls())
@@ -17,6 +19,21 @@ source('./Code/MyFunctions.R') # for Mode fxn
 
 all <- readRDS('./Data/Derived-data/DFs/all.Rds') # all data (df)
 ch2 <- readRDS('./Data/Derived-data/DFs/bears_ch2_110622.Rds') # study data
+den <- read.csv('./Data/Denning_locs_dates.csv') # denning data from TA
+
+# --- PREP DATA ------------------- #
+
+# TA
+
+den$entrance <- mdy(den$entrance)
+den$year <- year(den$entrance)
+
+den2 <- den %>% # add column for id
+  mutate(X = str_trim(X, side = "right")) %>% # remove whitespace
+  unite("id", X, year, sep = '.') %>%
+  filter(id %in% ids)
+
+# Ch2 data
 
 dbears <- ch2 %>%
   group_by(id) %>%
@@ -25,11 +42,11 @@ dbears <- ch2 %>%
 
 ids <- unique(dbears$id)
 
-# --- PREP DATA ------------------- #
-
 dbears <- data.frame(id = character(), 
                      denLat = double(), 
                      denLon = double())
+
+#   --------- GET DENNING LOCATIONS ----------------------------------  #
 
 # Get denning locations using Mode method
 
@@ -53,28 +70,40 @@ t <- all %>%
 t <- t %>%
   group_by(id) %>%
   mutate(den_location = 
-           if_else(gps_lat == denLat & gps_lon == denLon, 1, 0)) %>%
-  select(-c('denLat', 'denLon')) %>%
-  right_join(all)
+           if_else(gps_lat == denLat & gps_lon == denLon, 1, 0)) #%>%
+  #select(-c('denLat', 'denLon')) %>%
 
 
-db <- t %>%
-  filter(id %in% ids & month > 9) %>%
+time <- t %>%
   group_by(id) %>%
   arrange(id, datetime) %>%
   mutate(time.den=ifelse(den_location==0 | is.na(lag(den_location)) | lag(den_location)==0, 
                           0,
                           difftime(datetime, lag(datetime), units="days"))) %>% 
-  mutate(cumtime.den=time.den + ifelse(is.na(lag(time.den)), 0, lag(time.den)))
-  
-db %>%
-  filter(den_location == 1)
-  
-  
+  mutate(cumtime.den=cumsum(time.den))
 
-# Make sf
+# Create new df for manual inspection and addition of denning start dates
+  
+dstart <- time %>% 
+  filter(den_location == 1 & cumtime.den < 5)
+  
+dstart$enter_den <- NA  
 
-denLoc <- st_point(x = c(denLat, denLon))
-geom <- st_geometry(denLoc)
+dstart[1,34] <- 1  # 20333.2008
+dstart[38,34] <- 1 # 21015.2013
+dstart[91,34] <- 1 # 21368.2014
+dstart[87,34] <- 1 #21237.2011 - TA data says bear entered den 11-10-2011 but that was probably UTC or something. No data for that date. 
 
+ds <- dstart %>%
+  select(id, animal:second, den_location, enter_den)
+
+# ------------ ADD BACK TO MAIN DATAFRAME --------------------------- #
+
+all2 <- all %>%
+  left_join(ds) 
+
+all3 <- all2 %>%
+  replace_na(list(den_location = 0, enter_den = 0))
+
+#saveRDS(all3, './Data/Derived-data/DFs/all_11_06_2022.Rds')
 

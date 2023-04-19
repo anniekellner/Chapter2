4d0153f88bda77c978a265e02fdd2caad3ef3b06
anniekellner <annie.kellner@colostate.edu>
2023-04-18 22:01:22 -0600

    stopping for the night - need to check that all the non-denning, non-ice-departing bears actually lost their collars prior to Nov 1

--- Code/determine_study_end_dates.R
+++ Code/determine_study_end_dates.R
@@ -25,95 +25,83 @@ rm(list = ls())
 ch2 <- readRDS('./Data/Derived-data/DFs/bears_ch2_110622.Rds') # study data
 all <- readRDS('./Data/Derived-data/DFs/all_11_06_2022.Rds') # all data - has denning start dates
 
+all <- as_tibble(all)
+all$ymd <- ymd(all$ymd)
+
+ch2all <- ch2 %>%
+  left_join(all)
+
 ## Dates when bears enter dens
 
-ds <- all %>% filter(enter_den == 1)
-ds$ordinal <- yday(ds$ymd)
-summary(ds$ordinal)
+ch2all$Ordinal <- yday(ch2all$ymd)
 
-ds %>% arrange(ordinal)
+denDate <- ch2all %>%
+  filter(enter_den == 1) %>%
+  select(Ordinal)
+
+mean(denDate$Ordinal) # 307.25
+min(denDate$Ordinal) # 277
+max(denDate$Ordinal) #332
 
 ## Dates when bears leave for ice
 
-ice <- all %>% filter(departure_to_ice == 1)
+ice <- ch2all %>% filter(departure_to_ice == 1) # 7 observations
 
 ice$ordinal <- yday(ice$ymd)
 summary(ice$ordinal)
 
-ice %>% arrange(ordinal)
 
 # ---- ADD STUDY_END COLUMN TO DATAFRAME AND REMOVE POINTS AFTERWARD  ------------------ #
+
+# not sure where study_end dates came from, so deleting and re-doing. Many dates in November/December 
+
+ch2all <- ch2all %>%
+  select(!study_end)
+
+ch2all <- ch2all %>%
+  group_by(id) %>%
+  mutate(study_end = ifelse(departure_to_ice == 1 | enter_den == 1, 1, 0)) 
+
+# Check
+table(ch2all$study_end) # 11
+
+## ----------  Collar drops/malfunctions ------------------- ##
+
+iceden <- ch2all %>%
+  filter(study_end == 1) 
+
+icedenIDS <- unique(iceden$id)
+  
+others <- ch2all %>%
+  filter(!(id %in% icedenIDS))
+
+otherIDS <- unique(others$id)
+
+collarDrops <-  others %>% # Add study_end = 1 for bears whose collars dropped (data ends in September or October)
+  group_by(id) %>% 
+  arrange(id, datetime) %>%
+  slice_tail() %>%
+  mutate(study_end = replace(study_end, month == 9 | month == 10, 1))
+
+collarDropIDS <- collarDrops %>% group_by(id) %>% filter(study_end == 1) 
+unique(collarDropIDS$id)
+
 
 all2 <- all %>%
   group_by(id) %>%
   mutate(study_end = if_else(departure_to_ice == 1 | enter_den == 1, 1, 0)) %>%
   mutate(row = row_number()) %>%
   mutate(across(everything(),
-                ~replace(., row > match(1, study_end), NA))) %>%
-  drop_na(animal) %>% # arbitrary column that does not usually have an NA for any reason
+                ~replace(., row > match(1, study_end), NA))) %>% # remove points for bears after which study_end = 1
+  drop_na(animal) %>% # arbitrary column that does not usually have an NA for any reason - this removes rows after study_end rather than fill with NA
   ungroup()
 
-# Check
-table(all2$study_end) # 12 bears depart to ice or enter dens
-table(all2$enter_den)
 
-## ------------------- Add missing rows to ch2 df ---------------------------------- ##
 
-iceden <- all2 %>%
-  filter(study_end == 1)
-
-icedenIDS <- unique(iceden$id)
-
-addback <- all2 %>%
-  filter(id %in% icedenIDS & month > 9) 
-  
-ch2Cols <- colnames(ch2)
-addbackCols <- colnames(addback)
-
-setdiff(addbackCols, ch2Cols) # finds all rows in x that are not in y
-
-## Prep df to join back into ch2 df
-
-addback$ymd <- ymd(addback$ymd)
-
-addback <- addback %>%
-  select(-c('swim', 'SIC', 'ows', 'land_bear_ows', "land_bear", 'id.ymd', 'row', 'start.swim', 'end.swim'))
-
-ch2 <- ch2 %>%
-  full_join(addback)
-
-#studyEnd <- ch2 %>% group_by(id) %>% arrange(datetime) %>% slice_tail()  # looks good
-
-## ----------  Collar drops/malfunctions ------------------- ##
 
 # 7 bears whose collars appeared to drop off or malfunction
 
-other <- ch2 %>%
-  filter(!(id %in% icedenIDS))
 
-# Check back to all to see if more data is available
-
-otherIDS <- unique(other$id)
-
-otherAll <- all2 %>%
-  filter(id %in% otherIDS & month > 8)
-
-otherAll <-  otherAll %>% # Add study_end = 1 for bears whose collars dropped (data ends in September or October)
-  group_by(id) %>% 
-  arrange(id, datetime) %>%
-  slice_tail() %>%
-  mutate(study_end = 
-           if_else(month == 10 | month == 9, 1, study_end)) 
-
-drop <- otherAll %>%
-  filter(study_end == 1) %>%
-  mutate(ordinal = yday(ymd)) %>%
-  arrange(ordinal)
-
-summary(drop$ordinal)
-
-drop <- drop %>%
-  select(-c('swim', 'SIC', 'ows', 'land_bear_ows', "land_bear", 'id.ymd', 'row', 'start.swim', 'end.swim'))
 
 ## Add back to all df
 

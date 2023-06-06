c7fe17b9d9ba2731f4720a2f15aaf3c281ec1fc5
anniekellner <annie.kellner@colostate.edu>
2023-06-06 11:19:31 -0600

    joining dataframes by datetime requires using as.Posixct before datetime. Also df's will not join if milliseconds are different. see https://stackoverflow.com/questions/48829462/r-dplyrleft-join-using-datetime-columns-does-not-join-properly

---
+++ Code/join_dataframes.R
@@ -0,0 +1,31 @@
+###########################################
+###     JOIN ALL DF TO CH2 DF   ###########
+###########################################
+
+b <- readRDS('./Data/Derived-data/DFs/bears_ch2_052823.Rds')
+all <- readRDS("Data/raw_csv.Rds")
+
+all <- all %>%
+  dplyr::select(-c(data_origin, ptt:n_sats)) %>%
+  mutate(id = paste(animal, year, sep = '.')) %>%
+  mutate(hms = paste(hour, minute, second, sep = ':'))
+
+all <- all %>%
+  mutate(datetime = paste(date, hms))  
+
+all$datetime <- mdy_hms(all$datetime, tz = "US/Alaska")  
+
+
+t <- all %>%
+  filter(id == "pb_06810.2008") %>%
+  filter(datetime > "2008-09-03 00:00:00")
+
+t <- dplyr::filter(t, datetime == as.POSIXct("2008-09-03 00:00:43"))
+
+#saveRDS(all, './Data/raw_csv.Rds')
+
+pb <- b %>%
+  left_join(all, by = c('id', 'datetime')) %>%
+  mutate(gps_lat = coalesce(gps_lat)) %>%
+  mutate(gps_lon = coalesce(gps_lon)) %>%
+  mutate(date = coalesce(date))

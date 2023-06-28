a22ba563001640cdfef58ecb5eea8614c7724fca
anniekellner <annie.kellner@colostate.edu>
2023-06-28 11:43:04 -0600

    Summaries complete for tracking data

--- Code/for_USGS.R
+++ Code/for_USGS.R
@@ -45,19 +45,47 @@ ltraj <- as.ltraj(bdf[,c("X","Y")], date=bdf$datetime, id=bdf$id, typeII = TRUE)
 
 summary <- summary(ltraj)
 
+# Summaries of departure and arrival dates
+
+summary <- summary %>%
+  mutate(start_ordinal = yday(date.begin)) %>%
+  mutate(end_ordinal = yday(date.end))
+
+# Summaries of days tracked
+
 summary$DaysTrack <-round(difftime(summary$date.end, summary$date.begin, units="days"),digits=1)
+
+summary$days_tracked <- as.numeric(summary$days_tracked) # need to change var name
+summary(summary$days_tracked)
+
+# Summaries of start and end dates
+
+summary <- summary %>%
+  mutate(start_ordinal = yday(date.begin)) %>%
+  mutate(end_ordinal = yday(date.end))
+
+summary(summary$start_ordinal)
+summary(summary$end_ordinal)
+
+sd(summary$start_ordinal)
+sd(summary$end_ordinal)
+
+# Create table
 
 summary <- summary %>%
   mutate(year = year(date.begin)) %>%
   mutate(date.begin = as.character(date.begin)) %>%
   mutate(date.end = as.character(date.end)) %>% 
   separate_wider_delim(date.begin, " ", names = c("start_date", NA)) %>%
-  separate_wider_delim(date.end, " ", names = c("end_date", NA))
+  separate_wider_delim(date.end, " ", names = c("end_date", NA)) 
 
 summary <- summary %>%
   select(year, id, start_date, end_date, DaysTrack,) %>%
   rename(days_tracked = DaysTrack) %>%
+  mutate(days_tracked = as.numeric(days_tracked)) %>%
   arrange(year) %>% print(n = 21)
+
+# Table appears in Viewer (using gt package for table viz)
 
 summary %>%
   gt() %>%

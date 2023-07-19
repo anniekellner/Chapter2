94cce1012914505c75b40b5a19932e6ce8771277
anniekellner <annie.kellner@colostate.edu>
2023-07-19 17:51:45 -0600

    attempted to project DEM and points into polar stereographic but failed. Takes too long and not worth it.

--- Code/compare_sample_to_Pagano_etal.R
+++ Code/compare_sample_to_Pagano_etal.R
@@ -9,7 +9,12 @@
 library(tidyverse)
 library(sf)
 library(terra)
+library(leaflet)
 library(conflicted)
+
+conflicts_prefer(
+  dplyr::filter()
+)
 
 rm(list = ls())
 
@@ -17,6 +22,8 @@ rm(list = ls())
 
 pag <- read_csv('./Data/Pagano_bears.csv')
 me <- readRDS('./Data/Derived-data/DFs/bears_ch2_052823.Rds')
+
+all_v2 <- readRDS("C:/Users/akell/OneDrive - Colostate/PhD/Polar_Bears/Repos/Chapter2/Data/Derived-data/DFs/Old/all_v2.Rds")
 
 me <- me %>%
   select(animal, year) %>%
@@ -88,9 +95,30 @@ b7days <- b7days %>%
 
 # ------- CHECK METHODS FOR EXCLUDING THESE BEARS   --------------------- #
 
-b2sf <- st_as_sf(b2, coords = c('X','Y'), crs = 3338)
-  
-dem <- rast('./Data/Spatial/ans_dem_8bit.tif')
+b2sf <- st_as_sf(b2, coords = c('gps_lon','gps_lat'), crs = 4326)
+b2vec <- vect(b2sf)
 
+# DEM
   
+dem <- rast('./Data/Spatial/ans_dem_8bit.tif') # AK Albers Equal Area Conic
 
+rec <- st_read('./Data/Spatial/Rectangle/rectangle.shp')
+rec <- vect(rec)
+rec <- project(rec, dem)
+
+dem <- crop(dem, rec)
+
+# Project points into DEM projection
+
+pts <- project(b2vec, dem)
+
+
+
+writeRaster(dem, filename = 'C:/Users/akell/ArcGIS/Chapter2/DEM/fromR_check_water_071923.tif') # Check in ArcGIS what the code for water is
+
+
+
+b2vec <- project(b2vec, polar.stereo)
+
+plot(dem)
+points(b2vec, col = "black")

f0c6db9cd6676b31f0e52137ed968067e3de2964
anniekellner <annie.kellner@colostate.edu>
2023-08-01 14:05:43 -0600

    getting close but still haven't determined landfall for missing bears

--- Code/Determine_study_start_dates_OG.R
+++ Code/Determine_study_start_dates_OG.R
@@ -31,11 +31,12 @@ missing %>%
   filter(end.swim == 1) # 20446.2009, 20529.2005, 21264.2011, 21358.2013, 20529.2004
 
 ice <- missing %>%
-  filter(id == "pb_20446.2009" | 
-           id == "pb_20529.2005" | 
-           id == "pb_21264.2011" | 
-           id == "pb_21358.2013" | 
-           id == "20529.2004")
+  filter(id == "pb_20446.2009" | # included
+           id == "pb_20529.2005" | # included
+           id == "pb_21264.2011" | # included
+           id == "pb_21358.2013" | # included
+           id == "pb_20529.2004") %>%
+  filter(month > 6 & month < 11)
 
 # Spatial
 
@@ -51,25 +52,56 @@ buff5k <- demPoly_5k_crop %>%
 
 # -------   BEARS THAT ARRIVED FROM ICE ---------------- #
 
+iceSF <- st_as_sf(ice, coords = c('gps_lon', 'gps_lat'), crs = 4326)
+iceSF <- st_transform(iceSF, st_crs(buff5k))
 
+iceSF$Land <- st_intersects(iceSF, buff5k) %>% lengths > 0 # https://stackoverflow.com/questions/49294933/r-convert-output-from-sfst-within-to-vector
 
+iceSF <- iceSF %>% # remove previous column for land
+  select(-land) 
 
-
-# Spatial data
-
-b2sf <- st_transform(b2sf, st_crs(buff5k))
-
-test <- b2sf %>%
-  mutate(Land = if_else(st_intersects(., buff5k) == TRUE, 1, 0))
-
-b2sf$Land <- st_intersects(b2sf, buff5k) %>% lengths > 0 # https://stackoverflow.com/questions/49294933/r-convert-output-from-sfst-within-to-vector
 
 ## 7-day criteria
 
-lb <- b2sf %>% 
-  group_by(ID, ymd) %>%
+lb <- iceSF %>% 
+  group_by(id, ymd) %>%
   mutate(on_land = any(Land == TRUE)) %>%
-  mutate(consec_seven = rollapply(on_land, 7, all, align = 'left', fill = NA)) %>%
-  filter(consec_seven == TRUE)
+  ungroup()
+
+lb <- lb %>%
+  mutate(on_land = if_else(
+    on_land == TRUE, 1, 0)) 
+
+lb <- lb %>%
+  group_by(id, ymd) %>%
+  slice_head() %>%
+  ungroup()
+
+lb <- lb %>%
+  group_by(id) %>%
+  mutate(cum_land = cumsum(on_land)) 
+
+day7 <- lb %>% filter(cum_land == 7) %>% select(id, ymd)
+
+day1 <- day7 %>%
+  mutate(Day1 = ymd - ddays(7)) %>%
+  select(id, geometry, Day1) %>%
+  rename(ymd = Day1) %>%
+  st_drop_geometry()
+
+ice2 <- iceSF %>%
+  st_drop_geometry()
+
+landfall <- day1 %>%
+  left_join(ice2, by = c("id", "ymd"))
+
+landfall <- landfall %>%
+  group_by(id) %>%
+  arrange(id, datetime) %>%
+  filter(Land == TRUE) %>%
+  slice_head()
+  
+ 
+
 
 

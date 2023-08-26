a6e787677910d2665b82a1cf30b12f10906b75e0
anniekellner <annie.kellner@colostate.edu>
2023-08-26 11:21:12 -0600

    revised Pag start dates to reflect 5-day post-capture period for bears collared on land

--- Code/revise_start_dates_for_Pag_bears_captured_on_land.R
+++ Code/revise_start_dates_for_Pag_bears_captured_on_land.R
@@ -10,6 +10,8 @@ conflicts_prefer(
   dplyr::filter()
 )
 
+rm(list = ls())
+
 # ----- LOAD AND PREP DATA  --------------- #
 
 # Load
@@ -30,36 +32,51 @@ pag[1,] <- "pb_06810.2008"
 
 #saveRDS(pag, here("Data", "Derived-data", "Pag_IDs.Rds"))
 
-# --  EXAMINE STUDY START DATES --------- #
+# --  REVISE START DATES--------- #
 
-b %>% group_by(id) %>% select(id, datetime, study_start) %>% slice_head() %>% print(n = 28)
+# 5 calendar days after date of capture
 
-# 20965.2008 and 20975.2008 are Pag bears collared on land. Need to revise start dates. 
-
-# ----- CHANGE STUDY START TO REFLECT 5-DAY GRACE PERIOD FOR PAG BEARS CAPTURED ON LAND --------- #
-
-b <- b %>% 
-  select(-study_start)
-
-ice <- b %>%
-  filter(landfall == 1) 
-
-iceIDs <- unique(ice$id)
-
-landCollar <- b %>% 
-  filter(! id %in% iceIDs) %>%
+p <- b %>% # pag bears captured on land
+  filter(id == "pb_20965.2008" | id == "pb_20975.2008") %>%
+  select(id, ymd) %>%
   group_by(id) %>%
-  slice_head() %>%
-  mutate(study_start = 1) 
+  slice_head()
 
-b <- b %>%
-  left_join(landCollar) %>% glimpse()
+p$start_date <- p$ymd + ddays(5)
+
+p <- p %>%
+  select(-ymd) %>%
+  rename(ymd = start_date) %>%
+  left_join(b) %>%
+  group_by(id) %>%
+  slice_head() 
+
+b <- b %>% # 32017 rows
+  mutate(study_start = case_when(
+    id == "pb_20965.2008" & datetime == '2008-08-29 00:00:00' | 
+      id == "pb_20975.2008" & datetime == '2008-08-27 00:00:00' ~ 1,
+    TRUE ~ study_start)) %>% glimpse()
+
+# -- REMOVE DATES PRIOR TO STUDY START FOR PAG BEARS  ---- #
+  
+b <- b %>% # every variable becomes NA prior to landfall (except id which is grouping variable) # 32017
+  group_by(id) %>%
+  mutate(across(everything(),
+                ~replace(., row_number() < match(1, study_start), NA))) %>%
+  ungroup()
+
+noID <- b %>% # Remove rows where all vars are NA but ID # 31893 rows
+  select(animal:study_start) %>%
+  filter(if_any(everything(), ~ !is.na(.)))
+
+b <- b %>% # Put ID back in dataframe 
+  right_join(noID) # 31893 rows: looks good
+
+#saveRDS(b, here("Data", "Derived-data", "DFs", "OG_ch2_082623.Rds"))
 
 
 
 
-%>%
-  mutate(study_start == 1) %>%
-  mutate(landfall == 0)
+
 
 

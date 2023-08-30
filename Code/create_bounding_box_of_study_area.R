94a0fc993b126eff2c19c426e6f6528d8f7b7406
anniekellner <annie.kellner@colostate.edu>
2023-08-30 10:46:31 -0600

    created bounding box for study and removed points in Canada. Only 46 points total removed.

--- Code/create_bounding_box_of_study_area.R
+++ Code/create_bounding_box_of_study_area.R
@@ -5,28 +5,46 @@
 # Create bounding box of study area
 # Remove points east of US-CA border
 
+# Only 46 points total removed across all bear-summers
+
 library(sf)
 library(tmap)
+library(tmaptools)
 library(dplyr)
+library(here)
 
 rm(list = ls())
 
-pb <- readRDS('./Data/bears_092921.Rds')
+# ---- LOAD DATA ------ #
 
-pb <- st_transform(pb, 4326)
+pb <- readRDS(here("Data", "Derived-data", "DFs", "OG", "OG_082823.Rds")) # dataframe
 
-coords <- st_coordinates(pb)
-pb <- cbind(pb, coords)
+# ----  REMOVE POINTS WEST OF AK-CA BORDER  ----- #
 
-pb <- filter(pb, X < -141)
+pb2 <- filter(pb, gps_lon < -141) # 141st Meridian
+
+pbsf <- st_as_sf(pb2, coords = c("gps_lon", "gps_lat"), crs = 4326)
+
+pbsf <- cbind(st_coordinates(pbsf), pbsf) 
+
+# Check via plot 
 
 tmap_mode('view')
 
-tm_shape(pb) + 
-  tm_dots()
+tm_shape(pbsf) + 
+  tm_symbols(popup.vars = TRUE)
 
-bb = st_as_sfc(st_bbox(pb))
+bb <- st_as_sfc(st_bbox(pbsf))
 
-st_write(bb, './Data/Spatial/Output/bbox_for_heather.shp')
+# Save bounding box
+st_write(bb, here("Data", "Spatial", "Derived", "Bounding_boxes", "West_of_141.shp"))
 
-saveRDS(pb, './Data/bears_092921.Rds')
+pb3 <- st_drop_geometry(pbsf) # save as df
+
+# Rename X and Y to lat/lon so can add Albers when needed
+
+pb3 <- pb3 %>%
+  rename(gps_lon = X) %>%
+  rename(gps_lat = Y)
+
+#saveRDS(pb3, here("Data", "Derived-data", "DFs", "OG", "OG.Rds"))

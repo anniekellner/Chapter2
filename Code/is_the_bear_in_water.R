19cd90f0d334cd5118841a77a5653e72cbad805d
Annie Kellner <annie.kellner@colostate.edu>
2023-12-12 22:43:16 -0700

    Need to re-do on_island before I can do in_water

--- Code/is_the_bear_in_water.R
+++ Code/is_the_bear_in_water.R
@@ -21,54 +21,41 @@ rm(list = ls())
 
 # --- Load Data -------------------- #
 
-uaSF <- readRDS(here("Data", "Derived-data", "DFs", "OG", "uaSF_091823.Rds"))
+uaSF <- readRDS(here("Data", "Derived-data", "DFs", "OG", "uaSF_12-12-23.Rds"))
 islands <- st_read(here("Data", "Spatial", "Barrier_Islands", "islands_w_1500m_buffer.shp"))
 
 #   ----    CHECK NA'S    ----------    #
 
-nas <- filter(uaSF, is.na(elevation))
+nas <- filter(uaSF, is.na(elevation)) # in GEE, I replaced values less than 0 with 0 (ie nearshore points) but did not account for NA pts
 
 tmap_mode('view')
 
 tm_shape(nas) + 
   tm_symbols()
 
+# Replace NA's with 0
+
+uaSF <- uaSF %>%
+  replace_na(list(elevation = 0))
+
+
 # -- Assign Water ---------------- #
 
 uaSF <- uaSF %>%
   mutate(in_water = ifelse(on_island == 0 & elevation > 0, 0, 1))
 
-bone2 <- bone %>%
-  mutate(in_water = ifelse(on_island == "TRUE" | elevation > 0, 0, 1))
-
-
 # -- Check  ----------- #
 
-# Randomly sample 5% of rows and make sf object to plot
+# Randomly sample 1% of rows and make sf object to plot
 
-corr_samp <- corr2 %>%
-  sample_frac(size = .05, replace = FALSE) %>%
-  st_as_sf()
-
-bone_samp <- bone2 %>%
-  sample_frac(size = .05, replace = FALSE) %>%
-  st_as_sf(., coords = c('x_', 'y_'), crs = 3338)
-
-# Plot
-# Looks good! 5-12-2022
+samp <- slice_sample(uaSF, prop = 0.01)
 
 tmap_mode('view')
 
 tm_shape(islands) + 
-  tm_polygons(col = "green") +
-tm_shape(corr_samp) + 
-  tm_symbols(col = "in_water", popup.vars = c("elevation", "veg", "aspect", "on_island")) 
-
-tm_shape(islands) + 
-  tm_polygons(col = "green") +
-  tm_shape(bone_samp) + 
-  tm_symbols(col = "in_water") 
-
+  tm_polygons(fill = "green") +
+tm_shape(samp) + 
+  tm_symbols(col = "in_water", popup.vars = c("elevation", "aspect", "on_island")) 
 
 # Save
 

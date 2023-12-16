fe53a7b7a4e95cf7ff9ddcc58007ff0eb3578e0f
Annie Kellner <annie.kellner@colostate.edu>
2023-12-15 20:10:01 -0700

    redid with more complete df

--- Code/is_the_bear_on_an_island.R
+++ Code/is_the_bear_on_an_island.R
@@ -15,10 +15,9 @@ rm(list= ls())
 
 # --------- LOAD AND PREP DATA   ------------------------------------------------------- #
 
-ua <- readRDS(here("Data", "Derived-data", "DFs", "OG", "uaSF_12-12-23.Rds"))
+ua <- readRDS(here("Data", "Derived-data", "DFs", "OG", "allUA_12_15_23.Rds"))
 
-ua <- ua %>% # delete previous columns for on_island
-  select(-c(isl, on_island))
+uaSF <- st_as_sf(ua, coords = c("X","Y"), crs = 3338)
 
 islands <- st_read(here("Data", "Spatial", "Barrier_Islands", "islands_w_1500m_buffer.shp")) 
 
@@ -35,20 +34,22 @@ tm_shape(islands) +
 
 ## Intersection
 
-on_island <- lengths(st_intersects(ua, islands)) > 0 # creates a vector of whether or not point is on island
+on_island <- lengths(st_intersects(uaSF, islands)) > 0 # creates a vector of whether or not point is on island
 
-ua <- cbind(ua, on_island)
+ua <- cbind(uaSF, on_island)
 
 # Check  
 
 sample1 <- slice_sample(ua, prop = .01, replace = FALSE) # randomly select 1% of observations
 
+tmap_mode('view')
+
 tm_shape(islands) + 
   tm_polygons(fill = "green") + 
   tm_shape(sample1) + 
-  tm_symbols(size = 0.25, col = "on_island") # looks good 12-13-23
+  tm_symbols(size = 0.25, col = "on_island") # looks good 12-15-23
 
 
 # ----------- Save data ------------------------------ #
 
-#saveRDS(ua, here("Data", "Derived-data", "DFs", "OG", "uaSF_12-13-23.Rds")) # save as spatial object
+#saveRDS(ua, here("Data", "Derived-data", "DFs", "OG", "uaSF_12-15-23.Rds")) # save as spatial object

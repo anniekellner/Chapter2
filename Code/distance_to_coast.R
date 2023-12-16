0146649a2c6d48afa149abb1fee8d02b3b0230aa
Annie Kellner <annie.kellner@colostate.edu>
2023-12-15 18:57:17 -0700

    distance to coast works using terra. Need to add timestamp to dataframe

--- Code/distance_to_coast.R
+++ Code/distance_to_coast.R
@@ -5,13 +5,16 @@
 ## Only calculating distance to coast for mainland points (decided at meeting with GW 4/13/22)
 ## This metric is really only relevant for mainland points
 ## The positive/negative denotation messes with coefficients
+# Used terra package because sf was not working well
 
 # Coast = mainland coast. Digitized using combo of shapefile and IfSAR.
 
-library(dplyr)
+library(tidyverse)
 library(sf)
+library(terra)
 library(tmap)
 library(tmaptools)
+library(here)
 
 
 rm(list = ls())
@@ -20,24 +23,43 @@ source('./Code/MyFunctions.R')
 
 # ----- Load data ---------------------------------- #
 
+# Coastline
+
 coast <- st_read('./Data/Spatial/coastline/digitized_coastline.shp') # Used .shp of coastline as digitized from IFSAR images on GEE
 coast <- st_transform(coast, 3338)
 
-bone <- readRDS('./Data/Derived-data/bonepile_data.Rds')
-corr <- readRDS('./Data/Derived-data/corridor_data.Rds')
+coast_vec <- vect(coast) # convert coast to spatvector
+
+# Used and available pts
+
+uaSF <- readRDS(here("Data", "Derived-data", "DFs", "OG", "uaSF_12-13-23.Rds"))
+
+uaSF_noNA <- uaSF %>%
+  drop_na(elevation) # to reduce computational load, because these are water points anyway
+
+uaVec <- vect(uaSF_noNA)
+
 
 
 # --------  Calculate distance to coast  ------------------------- #
 
-corr <- corr %>%
-  st_as_sf() %>%
-  st_set_crs(3338) 
-  
-corr <- corr %>%
-  mutate(dist_to_coast = st_distance(., coast)) # will not work without . 
-  
-bone.sf <- bone.sf %>%
-  mutate(dist_to_coast = st_distance(., coast))
+plot(uaVec)
+plot(coast_vec, add = TRUE)
+
+dist <- distance(uaVec, coast_vec, pairwise=FALSE, unit="m")
+
+uaSF_noNA <- cbind(uaSF_noNA, dist)
+
+# --------  Join with original df ------------------------------  #
+
+# Drop geometries
+
+uaSF <- st_drop_geometry(uaSF)
+uaSF_noNA <- st_drop_geometry(uaSF_noNA) 
+
+uaSF2 <- left_join(uaSF, uaSF_noNA)
+
+uaSF[122,]
 
 # --------  Replace points in water & on islands with NA ---------- #
 
